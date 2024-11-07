a89ff33470f87631c1bcb1bb3809b701fb257d25
TheShadowsPawn <146788718+TheShadowsPawn@users.noreply.github.com>
2024-11-06 19:28:27 -0600

    Update dragonslayer.js

--- dragonslayer.js
+++ dragonslayer.js
@@ -1,5 +1,22 @@
-console.log("Dragonslayer System Loaded");
-
 Hooks.on("init", function() {
     console.log("Dragonslayer | Initializing");
+
+    // Register the actor sheet
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("dragonslayer", DragonslayerActorSheet, {
+        types: ["character"],
+        makeDefault: true
+    });
 });
+
+class DragonslayerActorSheet extends ActorSheet {
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["dragonslayer", "sheet", "actor"],
+            template: "systems/dragonslayer/templates/actor/character-sheet.html",
+            width: 600,
+            height: 600,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }]
+        });
+    }
+}
