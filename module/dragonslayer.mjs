7840e9ce7b6be0400d476768aedcc93f1a52a924
TheShadowsPawn <146788718+TheShadowsPawn@users.noreply.github.com>
2024-11-06 18:36:10 -0600

    Delete module directory

--- module/dragonslayer.mjs
+++
@@ -1,7 +0,0 @@
-Hooks.on("init", () => {
-    console.log("Dragonslayer | Initializing");
-
-    // Register sheet application classes
-    Actors.unregisterSheet("core", ActorSheet);
-    Items.unregisterSheet("core", ItemSheet);
-});
